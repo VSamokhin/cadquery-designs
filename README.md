c9808742b1580737cba0379e56369c0f752c17dc
KernelPanic <wowyupiyo@gmail.com>
2025-09-08 15:01:56 +0200

    Some refinments to make the cases robust

---
+++ README.md
@@ -0,0 +1,13 @@
+# CadQuery Designs Aimed for 3D-Printing
+
+## Requirements
+* `cadquery` – install it with miniforge/conda for a hassle-free setup
+* I use VSCode + OCP CAD Viewer plugin, but `CQ-editor` should work as well
+
+## phono-preamp-psu.py
+> [!NOTE]
+> This design is still in development, as soon as it's ready and test prints done, I'll add also a corresponding BOM.
+
+DIY phono-preamp and separate PSU cases. In my setup the PSU unit is only a 15-0-15V transformer, becasue rectifier circuit is > built into the phono-preamp.
+
+For a better noise protection the phono-preamp case should be shielded from inside using an adhesive copper foil, connected finally to the ground terminal.
--- phono-preamp-psu.py
+++ phono-preamp-psu.py
@@ -35,8 +35,8 @@ from ocp_vscode import show_object
 import os
 
 # Toggle exports (set True when you want STL/STEP files written)
-DO_STL_EXPORT = True
-DO_STEP_EXPORT = True
+DO_STL_EXPORT = False
+DO_STEP_EXPORT = False
 
 # Configuration (tweak if needed)
 WIDTH_PREAMP, LENGTH_PREAMP, HEIGHT = 120.0, 150.0, 50.0
@@ -45,9 +45,6 @@ BASE_WALL_THICK = 3.0       # generic wall thickness
 BASE_BOTTOM_THICK = 3.0     # bottom plate thickness
 LID_TOP_THICK = 3.0         # lid top plate thickness
 LID_WALL_THICK = BASE_WALL_THICK   # side wall thickness attached to lid
-PROTRUSION_DEPTH = LID_WALL_THICK  # ledge depth where lid sits in
-PEG_DIAMETER = 5.0
-PEG_HEIGHT = 3.0
 SCREW_DIAMETER = 3.2
 SCREW_CSK_DIAMETER = 6.0
 SCREW_CSK_ANGLE = 120
@@ -58,10 +55,15 @@ POWER_DIAMETER = 12.0 # diameter of power connector hole
 GND_DIAMETER = 8.0    # diameter of ground connector hole
 POWER_SOCKET_WIDTH = 48.0
 POWER_SOCKET_HEIGHT = 29.0
-BOSS_DIAMETER = 8.0
+BOSS_DIAMETER = 10.0
 
 # Utility: add vents as rectangular slots on a vertical wall workplane
-def add_vertical_wall_vents(wall: Workplane, area_length, area_height, wall_thick, spacing=6.0, slot_w=3.0, slot_h=None) -> Workplane:
+def add_vertical_wall_vents(wall: Workplane,
+                            area_length, area_height,
+                            wall_thick,
+                            spacing=6.0,
+                            slot_w=3.0,
+                            slot_h=None) -> Workplane:
     if slot_h is None:
         slot_h = max(4.0, area_height - 4.0)
 
@@ -76,7 +78,15 @@ def add_vertical_wall_vents(wall: Workplane, area_length, area_height, wall_thic
             .rect(slot_w, slot_h)
             .cutBlind(-wall_thick))
 
-def add_diagonal_wall_vents(wall: Workplane, area_length, area_height, wall_thick, width, base_thick=BASE_BOTTOM_THICK, height=HEIGHT, spacing=6.0, slot_w=3.0, slot_h=None, angle=45.0) -> Workplane:
+def add_diagonal_wall_vents(wall: Workplane,
+                            area_length, area_height,
+                            wall_thick, width,
+                            height=HEIGHT,
+                            base_thick=BASE_BOTTOM_THICK,
+                            spacing=6.0,
+                            slot_w=3.0,
+                            slot_h=None,
+                            angle=45.0) -> Workplane:
     """
     Add diagonal vents as rectangular slots on a vertical wall workplane.
     I'm lazy to find a better solution, so for to get proper slots in >X faces (symmetrical to those in <X faces),
@@ -106,7 +116,11 @@ def add_diagonal_wall_vents(wall: Workplane, area_length, area_height, wall_thic
     return wall.cut(cuts)
 
 # Utility: add bottom mounting holes (for brass bosses) on bottom plate outer face
-def add_bottom_mounts(bottom_plate: Workplane, spacing_x, spacing_y, thru_dia=SCREW_DIAMETER, csk_dia=BRASS_CSK_DIAMETER, csk_angle=BRASS_CSK_ANGLE) -> Workplane:
+def add_bottom_mounts(bottom_plate: Workplane,
+                      spacing_x, spacing_y,
+                      thru_dia=SCREW_DIAMETER,
+                      csk_dia=BRASS_CSK_DIAMETER,
+                      csk_angle=BRASS_CSK_ANGLE) -> Workplane:
     hx = spacing_x / 2.0
     hy = spacing_y / 2.0
     pts = [(hx, hy), (-hx, hy), (hx, -hy), (-hx, -hy)]
@@ -122,140 +136,146 @@ def add_internal_bosses(base: Workplane, lid: Workplane,
                         height=HEIGHT,
                         boss_dia=BOSS_DIAMETER,
                         hole_dia=SCREW_DIAMETER,
-                        inset_x=LID_WALL_THICK,
-                        inset_y=BASE_WALL_THICK,
+                        lid_wall_thick=LID_WALL_THICK,
+                        base_wall_thick=BASE_WALL_THICK,
                         bottom_thick=BASE_BOTTOM_THICK,
                         csk_dia=SCREW_CSK_DIAMETER,
                         csk_angle=SCREW_CSK_ANGLE) -> tuple[Workplane, Workplane]:
-    # base is the assembled base solid (bottom + walls). We'll place bosses on top of the bottom plate inside
-    hx = width / 2.0 - inset_x - boss_dia / 2.0
-    hy = length / 2.0 - inset_y - boss_dia / 2.0 + 1.0
-    pts = [(hx, hy), (-hx, hy), (hx, -hy), (-hx, -hy)]
+    melt_depth = 1.0
+    hx = width / 2.0 - lid_wall_thick - boss_dia / 2.0 + melt_depth
+    hy = length / 2.0 - lid_wall_thick - boss_dia / 2.0
+    pts = [(hx, hy + melt_depth), (-hx, hy + melt_depth), (hx, -hy + base_wall_thick), (-hx, -hy + base_wall_thick)]
+    hole_height = 10.0
     for (x, y) in pts:
         # create a cylinder rising from inside bottom surface
         boss = (cq
                 .Workplane("XY")
-                .transformed(offset=(x, y, 0))
+                .transformed(offset=(x, y, bottom_thick))
                 .circle(boss_dia / 2.0)
-                .extrude(height - bottom_thick / 2.0)
-                .faces(">Z")
+                .extrude(height - bottom_thick)
+                .faces("<Z")
                 .workplane(centerOption="CenterOfMass")
-                .hole(hole_dia, depth=20.0)) # hole for screw into boss (from lid screw down)
-        base = base.union(boss)
+                .hole(hole_dia, depth=hole_height)) # hole for screw into boss (from lid screw down)
+        lid = lid.union(boss)
 
-        lid = (lid
-               .faces(">Z")
-               .workplane(centerOption="CenterOfMass")
+        base = (base
+               .faces("<Z")
+               .workplane(origin=(0, lid_wall_thick + melt_depth, 0))
                .pushPoints(pts)
                .cskHole(diameter=hole_dia, cskDiameter=csk_dia, cskAngle=csk_angle))
 
     return base, lid
 
 # Build base: bottom plate + front and rear walls attached
-def build_base(width, length, bottom_thick=BASE_BOTTOM_THICK, wall_thick=BASE_WALL_THICK, wall_height=HEIGHT, top_thick=LID_TOP_THICK) -> Workplane:
+def build_base(width, length,
+               height=HEIGHT,
+               bottom_thick=BASE_BOTTOM_THICK,
+               top_thick=LID_TOP_THICK,
+               base_wall_thick=BASE_WALL_THICK,
+               lid_wall_thick=LID_WALL_THICK) -> Workplane:
     # bottom plate centered at origin, thickness bottom_thick
     bottom = (cq
               .Workplane("XY")
               .box(width, length, bottom_thick, centered=(True, True, False)))
 
-    front_rear_length = width - wall_thick * 2.0
-    front_rear_height = wall_height - top_thick / 2.0
-    # front wall (positive Y direction): place at y = + (length/2 - wall_thick/2)
-    front = (cq
-             .Workplane("XY")
-             .transformed(offset=(0, length / 2.0 - wall_thick / 2.0, 0))
-             .box(front_rear_length, wall_thick, front_rear_height, centered=(True, True, False)))
+    rear_length = width - lid_wall_thick * 2.0
+    wall_height = height - bottom_thick - top_thick / 2.0
 
-    # rear wall (negative Y): at y = - (length/2 - wall_thick/2)
+    # rear wall
     rear = (cq
             .Workplane("XY")
-            .transformed(offset=(0, -length / 2.0 + wall_thick / 2.0, 0))
-            .box(front_rear_length, wall_thick, front_rear_height, centered=(True, True, False)))
+            .transformed(offset=(0, -length / 2.0 + base_wall_thick / 2.0 + base_wall_thick, bottom_thick))
+            .box(rear_length, base_wall_thick, wall_height, centered=(True, True, False)))
 
-    return bottom.union(front).union(rear)
+    return bottom.union(rear)
 
 # Build lid: top plate + side walls attached
-def build_lid(width, length, top_thick=LID_TOP_THICK, side_thick=LID_WALL_THICK, wall_height=HEIGHT, bottom_thick=BASE_BOTTOM_THICK) -> Workplane:
+def build_lid(width, length,
+              height=HEIGHT,
+              top_thick=LID_TOP_THICK,
+              bottom_thick=BASE_BOTTOM_THICK,
+              wall_thick=LID_WALL_THICK) -> Workplane:
     # top plate centered at origin but later positioned at top; we'll create centered and translate when assembling
     top = (cq
            .Workplane("XY")
-           .transformed(offset=(0, 0, wall_height-top_thick/2.0))
+           .transformed(offset=(0, 0, height - top_thick))
            .box(width, length, top_thick, centered=(True, True, False)))
 
-    left_right_height = wall_height - bottom_thick
-    left_right_z_offset = top_thick
+    wall_height = height - bottom_thick / 2.0
+    wall_z_offset = bottom_thick / 2.0
+
+    # front wall (positive Y direction)
+    front = (cq
+             .Workplane("XY")
+             .transformed(offset=(0, length / 2.0 - wall_thick / 2.0, wall_z_offset))
+             .box(width - wall_thick * 2.0, wall_thick, wall_height, centered=(True, True, False)))
+
     # left side wall (negative X): create wall and position so it attaches to left edge of top
     left = (cq
             .Workplane("XY")
-            .transformed(offset=(-width/2.0 + side_thick/2.0, 0, left_right_z_offset))
-            .box(side_thick, length, left_right_height, centered=(True, True, False)))
+            .transformed(offset=(-width / 2.0 + wall_thick / 2.0, 0, wall_z_offset))
+            .box(wall_thick, length, wall_height, centered=(True, True, False)))
 
     # right side wall (positive X)
     right = (cq
              .Workplane("XY")
-             .transformed(offset=(width/2.0 - side_thick/2.0, 0, left_right_z_offset))
-             .box(side_thick, length, left_right_height, centered=(True, True, False)))
+             .transformed(offset=(width / 2.0 - wall_thick / 2.0, 0, wall_z_offset))
+             .box(wall_thick, length, wall_height, centered=(True, True, False)))
 
-    return top.union(left).union(right)
+    return top.union(front).union(left).union(right)
 
-# Add aligments (protrusions and corresponding ledges) in the bottom plate and the lid walls
-def add_aligments(base: Workplane, lid: Workplane, width, bottom_thick=BASE_BOTTOM_THICK, wall_thick=LID_WALL_THICK) -> tuple[Workplane, Workplane]:
-    alignment_length = 30.0
-    left_x_offset = -width / 2.0 + wall_thick / 2.0
-    right_x_offset = width / 2.0 - wall_thick / 2.0
-    base = (base
-            .faces("<Z")
-            .workplane(centerOption="CenterOfMass")
-            .moveTo(left_x_offset, 0)
-            .rect(wall_thick, alignment_length)
-            .cutBlind(until=-bottom_thick) # left protrusion
-            .moveTo(right_x_offset, 0)
-            .rect(wall_thick, alignment_length)
-            .cutBlind(until=-bottom_thick)) # right protrusion
+# Add aligments in the bottom and top plate
+def add_aligments(base: Workplane, lid: Workplane,
+                  bottom_thick=BASE_BOTTOM_THICK) -> tuple[Workplane, Workplane]:
+    base_cut, lid_cut = (base
+                         .intersect(lid)
+                         .faces("<Z")
+                         .workplane(-bottom_thick / 2.0)
+                         .split(keepTop=True, keepBottom=True)
+                         .all())
 
-    tolerance = 0.2
-    y_offset = -alignment_length / 2.0 + tolerance / 2.0
-    # left ledge (negative X)
-    left_ledge = (cq
-                  .Workplane("XY")
-                  .transformed(offset=(left_x_offset, y_offset, 0))
-                  .box(wall_thick, alignment_length - tolerance, bottom_thick, centered=(True, False, False)))
+    return base.cut(base_cut), lid.cut(lid_cut)
 
-    # right ledge (positive X)
-    right_ledge = (cq
-                  .Workplane("XY")
-                  .transformed(offset=(right_x_offset, y_offset, 0))
-                  .box(wall_thick, alignment_length - tolerance, bottom_thick, centered=(True, False, False)))
-
-    return base, lid.union(left_ledge).union(right_ledge)
-
-# Create rear wall cutouts (only on rear wall) for preamp
-def add_preamp_rear_connectors(base: Workplane, cinch_dia=CINCH_DIAMETER, power_dia=POWER_DIAMETER, ground_dia=GND_DIAMETER, wall_thick=BASE_WALL_THICK) -> Workplane:
+# Create rear wall cutouts for preamp
+def add_preamp_rear_connectors(base: Workplane,
+                               height=HEIGHT,
+                               cinch_dia=CINCH_DIAMETER,
+                               power_dia=POWER_DIAMETER,
+                               ground_dia=GND_DIAMETER,
+                               wall_thick=BASE_WALL_THICK) -> Workplane:
     # Place input pair centered at X = -30, power at X = 0, output pair at X = +30 (approx)
     cinch_interval = 15.0
-    cinch_offset = 30.0
+    cinch_x_offset = 30.0
+    z_offset = height / 2.0 + ground_dia / 2.0
     return (base
             .faces("<Y")
-            .workplane(offset=0, centerOption="CenterOfMass")
-            .pushPoints([(-cinch_interval/2.0 - cinch_offset, 0), (cinch_interval/2.0 - cinch_offset, 0)])
+            .workplane(offset=-wall_thick, centerOption="CenterOfMass")
+            .pushPoints([(-cinch_interval / 2.0 - cinch_x_offset, z_offset), (cinch_interval / 2.0 - cinch_x_offset, z_offset)])
             .hole(cinch_dia, depth=wall_thick)  # Input pair
-            .pushPoints([(0, -power_dia)])
-            .hole(ground_dia, depth=wall_thick)  # Ground terminal
-            .pushPoints([(0, 0)])
-            .hole(power_dia, depth=wall_thick) # Power in (center)
-            .pushPoints([(-cinch_interval/2.0 + cinch_offset, 0), (cinch_interval/2.0 + cinch_offset, 0)])
+            .pushPoints([(0, -cinch_interval + z_offset)])
+            .hole(ground_dia, depth=wall_thick) # Ground terminal
+            .pushPoints([(0, z_offset)])
+            .hole(power_dia, depth=wall_thick)  # Power in (center)
+            .pushPoints([(-cinch_interval / 2.0 + cinch_x_offset, z_offset), (cinch_interval / 2.0 + cinch_x_offset, z_offset)])
             .hole(cinch_dia, depth=wall_thick)) # Output pair
 
 # Create rear wall cutouts for PSU
-def add_psu_rear_connectors(base: Workplane, width_psu=WIDTH_PSU, power_dia=POWER_DIAMETER, socket_width=POWER_SOCKET_WIDTH, socket_height=POWER_SOCKET_HEIGHT, wall_thick=BASE_WALL_THICK) -> Workplane:
+def add_psu_rear_connectors(base: Workplane,
+                            width_psu=WIDTH_PSU,
+                            height=HEIGHT,
+                            power_dia=POWER_DIAMETER,
+                            socket_width=POWER_SOCKET_WIDTH,
+                            socket_height=POWER_SOCKET_HEIGHT,
+                            wall_thick=BASE_WALL_THICK) -> Workplane:
     hole_x_offset = 10.0
+    z_offset = height / 2.0
     return (base
             .faces("<Y")
-            .workplane(centerOption="CenterOfMass")
-            .moveTo(-width_psu/2.0 + socket_width/2.0 + hole_x_offset + wall_thick, 0)
+            .workplane(offset=-wall_thick, centerOption="CenterOfMass")
+            .moveTo(-width_psu/2.0 + socket_width/2.0 + hole_x_offset + wall_thick, z_offset)
             .rect(socket_width, socket_height)
             .cutBlind(until=-wall_thick)  # Power socket cutout
-            .pushPoints([(width_psu/2.0 - power_dia/2.0 - hole_x_offset - wall_thick, 0)])
+            .pushPoints([(width_psu/2.0 - power_dia/2.0 - hole_x_offset - wall_thick, z_offset)])
             .hole(power_dia, depth=wall_thick))  # Ground terminal hole below socket
 
 # Export functions
@@ -296,8 +316,8 @@ if __name__ == "__main__":
     # Build preamp lid (top + side walls)
     preamp_lid = build_lid(WIDTH_PREAMP, LENGTH_PREAMP)
 
-    # Add alignments
-    preamp_base, preamp_lid = add_aligments(preamp_base, preamp_lid, WIDTH_PREAMP)
+    # Make sure lid fits over base
+    preamp_base, preamp_lid = add_aligments(preamp_base, preamp_lid)
     # Add internal bosses for screws
     preamp_base, preamp_lid = add_internal_bosses(preamp_base, preamp_lid, WIDTH_PREAMP, LENGTH_PREAMP)
 
@@ -305,7 +325,7 @@ if __name__ == "__main__":
 
     # Build and assemble PSU case
     psu_base = build_base(WIDTH_PSU, LENGTH_PSU)
-    psu_base = add_bottom_mounts(psu_base, spacing_x=65.0, spacing_y=70.0)
+    psu_base = add_bottom_mounts(psu_base, spacing_x=70.0, spacing_y=65.0)
     psu_base = add_psu_rear_connectors(psu_base)
 
     # Build PSU lid (top + side walls)
@@ -320,8 +340,8 @@ if __name__ == "__main__":
     #psu_lid = add_vertical_wall_vents(psu_lid, psu_vent_length, psu_vent_height, LID_WALL_THICK)
     psu_lid = add_diagonal_wall_vents(psu_lid, psu_vent_length, psu_vent_height, -LID_WALL_THICK, -WIDTH_PSU, angle=-45.0)
 
-    # Add alignments
-    psu_base, psu_lid = add_aligments(psu_base, psu_lid, WIDTH_PSU)
+    # Make sure lid fits over base
+    psu_base, psu_lid = add_aligments(psu_base, psu_lid)
     # Add internal bosses for screws
     psu_base, psu_lid = add_internal_bosses(psu_base, psu_lid, WIDTH_PSU, LENGTH_PSU)
 
