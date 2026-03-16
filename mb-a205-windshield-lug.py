87948afd9860f93df0094497089a609130e38844
KernelPanic <wowyupiyo@gmail.com>
2026-03-16 11:50:21 +0100

    Make holes more precise, add a mirroed part

--- mb-a205-windshield-lug.py
+++ mb-a205-windshield-lug.py
@@ -51,15 +51,17 @@ CUT_OFF_H = 5.0        # Z
 
 EAR_TH = 5.0           # Thickness of ear in X
 EAR_HOLE_D = 6.0       # Hole diameter
+EAR_CSK_D = EAR_HOLE_D + 1.5
 EAR_OUTER_R = 7.0      # Total length of ear from apex to the farest point of circle is 3 * R
 EAR_OFFSET_X = -5.0
-EAR_OFFSET_Y = 5.0
+EAR_OFFSET_Y = 2.0
 
 # Small rear latch/tab visible on photos (approximate)
 REAR_TAB_W = 7.0
 REAR_TAB_D = 3.0
 REAR_TAB_H = 4.0
 REAR_TAB_OFFSET_X = -4.0
+REAR_TAB_OFFSET_Y = 5.0
 REAR_TAB_FILLET = 0.95
 
 RABBET_W = 2.5
@@ -101,6 +103,10 @@ if __name__ == "__main__":
         .pushPoints([circle_center_yz])
         .circle(EAR_OUTER_R)
         .extrude(EAR_TH)
+        .faces(">X")
+        .workplane()
+        .pushPoints([(-2.0 * EAR_OUTER_R, 0.0)])
+        .cskHole(EAR_HOLE_D, EAR_CSK_D, 82.0)
         #.edges("|X and (>Z or <Z)")
         #.fillet(EAR_OUTER_RADIUS - 1.0)
     )
@@ -112,7 +118,7 @@ if __name__ == "__main__":
         cq.Workplane("XY", origin=(0.0, 0.0, BASE_H - CUT_OFF_H))
         .polyline([base_p1, (EAR_OFFSET_X, base_p1[1]), (EAR_OFFSET_X, base_p4[1]), base_p4])
         .close()
-        .polyline([(EAR_OFFSET_X, base_p1[1]), base_p2, (base_p2[0], EAR_OFFSET_Y), (EAR_OFFSET_X, EAR_OFFSET_Y)])
+        .polyline([(EAR_OFFSET_X, base_p1[1]), base_p2, (base_p2[0], REAR_TAB_OFFSET_Y), (EAR_OFFSET_X, REAR_TAB_OFFSET_Y)])
         .close()
         .extrude(CUT_OFF_H)
     )
@@ -123,11 +129,12 @@ if __name__ == "__main__":
     # Hole
     # =========================
     part = (
-        part.union(ear_profile)
+        part
         .faces("<X")
         .workplane()
-        .pushPoints([(2.0 * EAR_OUTER_R - EAR_OFFSET_Y, BASE_H)])
-        .hole(EAR_HOLE_D)
+        .pushPoints([(-EAR_OFFSET_Y + 2.0 * EAR_OUTER_R, BASE_H)])
+        .hole(EAR_CSK_D + 1.5)
+        .union(ear_profile)
     )
 
     # =========================
@@ -140,7 +147,7 @@ if __name__ == "__main__":
     tab_p5 = (REAR_TAB_D / 2.0, 0.0)
     tab_p6 = (0.0, 0.0)
     rear_tab = (
-        cq.Workplane("YZ", origin=(REAR_TAB_OFFSET_X, EAR_OFFSET_Y, BASE_H - CUT_OFF_H))
+        cq.Workplane("YZ", origin=(REAR_TAB_OFFSET_X, REAR_TAB_OFFSET_Y, BASE_H - CUT_OFF_H))
         .polyline([tab_p1, tab_p2, tab_p3, tab_p4, tab_p5, tab_p6])
         .close()
         .extrude(REAR_TAB_W)
@@ -185,7 +192,7 @@ if __name__ == "__main__":
 
     lug2 = part.mirror()
 
-    model = { "lug 1": part, "lug 2": lug2 }
+    model = { "MB a205 windshield lug 1": part, "MB a205 windshield lug 2": lug2 }
 
     # Optional export
     cq_utils.export_models(DO_STL_EXPORT, DO_STEP_EXPORT, **model)
